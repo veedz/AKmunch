5024599ec7e94ee9ea643e77da387f5de352047a
veedz <m.al.hafizd@gmail.com>
2026-01-21 21:37:39 +0700

    Update kernel string in anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Onion based N0Kontzzz by bimoalfarrabi (thanks to EmanuelCN & Impqxr)
+kernel.string=‎Morphite based N0Kontzzz by bimoalfarrabi (thanks to EmanuelCN & Impqxr)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
